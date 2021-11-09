c6b17e664f8ed05d807dedc84731f8622e5b0547
Fran <chuzzgomez18@gmail.com>
2021-11-09 12:02:03 -0600

    Se cambio a reservar 2

--- models/reservar_2.php
+++ models/reservar_2.php
@@ -59,5 +59,61 @@ if(!$enlace){
 }else{
 
     echo "Se mete al else";
+
+    
+    // Encontrar id de habitación
+    $actualizar = "UPDATE habitaciones SET disponible=0 WHERE tipo = '$numero'";
+    $datos = mysqli_query($enlace, $actualizar);
+    
+    if($datos){
+
+
+        $consultar_habit = "SELECT id_habitacion FROM habitaciones WHERE tipo = '$numero'";
+        $datos_consult = mysqli_query($enlace, $consultar_habit);
+        
+        foreach($datos_consult as $key){
+            $numero = $key['id_habitacion'];
+        }
+        if($numero != null){
+
+  
+
+            $insert = "INSERT INTO reservaciones (id_usuario, fecha_estancia, id_habitacion, personas) VALUES ($id_usuario,'$fecha', $numero , $personas)";
+            
+
+            $datos_insert = mysqli_query($enlace, $insert);
+
+            if($datos_insert){
+
+                $consultar_tarjet = "SELECT * FROM tarjetas WHERE id_usuario = $id_usuario";
+                $ejecutar_tarjet = mysqli_query($enlace, $consultar_tarjet);
+
+                if($ejecutar_tarjet){
+
+                    $rows_dato = mysqli_num_rows($ejecutar_tarjet);
+                    if($rows_dato == 0){
+                    
+                        $insert_tarjet = "INSERT INTO tarjetas (id_usuario,  num_tarjeta, fecha, codigo, postal) VALUE ($id_usuario, '$tarjeta_encript' ,'$fecha_vence', '$codigo_encript', $postal)";
+
+                        $datos_tarjet = mysqli_query($enlace, $insert_tarjet);
+                        
+                        if($datos_tarjet){
+                        
+                            header("Location: ../views/datos_cliente.php");
+                            
+
+                        }    
+                    }else{
+                        header("Location: ../views/datos_cliente.php");
+                    }
+                }
+
+               
+
+            }
+        }
+        
+        
+    }
     
 }
